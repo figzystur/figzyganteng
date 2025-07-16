e7a1bc73cbade48798817e02793c161ac002f33c
figzystur <alfigosinaga@gmail.com>
2025-07-16 17:55:06 +0700

    Update token

--- Sock.js
+++ Sock.js
@@ -1,5 +1,6 @@
 {
   "tokens": [
-    "7853084900:AAFcdyHRp-QkZ1rIstTomou4xMAROLFiEAw"
+    "7853084900:AAFcdyHRp-QkZ1rIstTomou4xMAROLFiEAw",
+    "7881802352:AAHSH-lh7S9zSrlOg2xm5vXg7aCNhyTT0c0"
   ]
 }
